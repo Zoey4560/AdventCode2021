c10f5d4a87760faa3aa8ed58d62fae8a74fbe908
zechw <zechwilliams@gmail.com>
2022-01-12 11:43:23 -0800

    3

---
+++ src/3.1.ts
@@ -0,0 +1,32 @@
+import {readFile} from 'fs'
+
+readFile('data/3.txt', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+
+    const values = data.toString().split(/\n/)
+    let valueCounts: number[][] = [] // [[a0, a1], [b0, b1]]
+
+    values.forEach(sval => {
+        sval.split('').forEach((char, i) => {
+            let val = parseInt(char)
+            if (valueCounts[i] == undefined) {
+                valueCounts[i] = []
+            }
+            if (valueCounts[i][val] == undefined) {
+                valueCounts[i][val] = 0
+            }
+            valueCounts[i][val] += 1
+        })
+    })
+
+    console.log('Value Counts:')
+    console.log(valueCounts)
+
+    let gamma = parseInt(valueCounts.map(c => c[0] > c[1] ? 0 : 1).join(''), 2)
+    let epsilon = gamma ^ 4095
+    console.log(`Gamma: ${gamma} Epsilon: ${epsilon}`)
+    console.log(`Power Usage: ${gamma * epsilon}`)
+})
---
+++ src/3.2.ts
@@ -0,0 +1,43 @@
+import {readFile} from 'fs'
+
+type Values = number[][]
+
+readFile('data/3.txt', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+
+    const values: Values = data.toString().split(/\n/).map(x => x.split('').map(y => parseInt(y)))
+    values.pop() //drop trailing newline
+
+    const oxygenRating = ratingReducer(values, false)
+    const co2Rating = ratingReducer(values, true)
+
+    console.log(`Oxy: ${oxygenRating} Co2: ${co2Rating}`)
+    console.log(`Power Usage: ${oxygenRating * co2Rating}`)
+})
+
+function countValues(values: Values, i: number) {
+    let valueCounts = [0, 0] // for digits abcd.. [[a0, a1], [b0, b1], ..]
+    values.forEach(vals => {
+        valueCounts[vals[i]] += 1
+    })
+    return valueCounts
+}
+
+function ratingReducer(sourceValues: Values, flip: boolean) {
+    let values = [...sourceValues]
+    let i = 0
+    while (values.length > 1) {
+        let c = countValues(values, i)
+        let testVal = c[0] > c[1] ? 0 : 1
+        if (flip) {
+            testVal = 1 - testVal
+        }
+        values = values.filter(x => x[i] == testVal)
+        // console.debug(`Run: ${i} Length: ${values.length}`)
+        i++
+    }
+    return parseInt(values[0].join(''), 2)
+}
