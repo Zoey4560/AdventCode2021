511e630ae6b0f369c017a3766e8532229d028478
zoey4560 <1054177+Zoey4560@users.noreply.github.com>
2022-01-13 01:59:59 -0800

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2021 attempts.
+## Focus on improving Typescript & Docker skills
+
+Local:
+  `tsc && npm start <# of puzzle to solve>``
+Docker:
+  `docker run --rm -it $(docker build -q .) <# of puzzle to solve>``
+
+
+
+## TODOs:
+  - docker - set up linked volumes
+  - docker - fix tsc install -- doesn't register? not global?
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,3 @@
-/************************************************
-  Advent of Code 2021 attempts.
-  Focus on improving Typescript & Docker skills
-
-  Local:
-    tsc && npm start <# of puzzle to solve>
-  Docker:
-    docker run --rm -it $(docker build -q .) <# of puzzle to solve>
-
-
-
-TODOs:
-    docker - set up linked volumes
-    docker - fix tsc install -- doesn't register?
-
-************************************************/
-
 const puzzle = process.argv[2]
 if (!puzzle){
     console.log('Advent Code Breaker! Please pick which number puzzle you\'d like solved')
