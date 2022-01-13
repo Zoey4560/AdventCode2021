f1656c2fa80ba76f4eb2e70c3473c12eb88ab799
zoey4560 <1054177+Zoey4560@users.noreply.github.com>
2022-01-12 20:33:01 -0800

    4.2

--- src/4.2.ts
+++ src/4.2.ts
@@ -5,20 +5,23 @@ import {BingoCard} from './models/Bingo'
 readData('4', (data) => {
     const [numbersData, ...boardsData] = data.split(/\n\n/)
     const numbers = numbersData.split(/,/).map(x => parseInt(x))
-    const boards = boardsData.map(x => new BingoCard(x))
+    let boards = boardsData.map(x => new BingoCard(x))
 
     for (let n = 0; n < numbers.length; n++) {
         const number = numbers[n]
         console.debug(`Calling #: ${number}`)
+
         for (let i = 0; i < boards.length; i++) {
             let board = boards[i]
             board.markNumber(number)
-            if (board.isWinning) {
-                console.debug('WINNER')
-                console.debug(board)
-                console.log(`Final Score: ${board.score * number}`)
-                return
-            }
         }
+
+        if (boards.length == 1 && boards[0].isWinning) {
+            console.log(`Final Score: ${boards[0].score * number}`)
+            return
+        }
+
+        boards = boards.filter(b => !b.isWinning)
     }
+    throw new Error('Winning Board not found.')
 })
